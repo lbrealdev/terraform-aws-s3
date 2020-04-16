9a8e4f7a7bb6329789f200ee3efc167c22b55f2b
Administrator <admin@example.com>
2020-04-16 11:24:14 +0200

    Add outputs module s3

---
+++ modules/s3_bucket/outputs.tf
@@ -0,0 +1,7 @@
+output "bucket_name" {
+  value = aws_s3_bucket.main.bucket
+}
+
+output "bucket_domain_name" {
+  value = aws_s3_bucket.main.bucket_domain_name
+}
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "this_bucket_name" {
+  value = module.s3_bucket.bucket_name
+}
+
+output "this_bucket_domain_name" {
+  value = module.s3_bucket.bucket_domain_name
+}
